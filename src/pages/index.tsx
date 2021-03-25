bcf740507c29feb5d61638d6a45a78c1752908ea
Alexandre Bitar <joabitar76@gmail.com>
2021-03-25 11:06:31 -0300

    removido estilos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import styles from '../styles/home.module.scss';
 import Head from 'next/head';
 
 export default function Home() {
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>inicio | ignews</title>
       </Head>
-      <h1 className={styles.title}>Hello Ignite</h1>
+      <h1>Hello Ignite</h1>
     </>
   )
 }
