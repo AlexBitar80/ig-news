9b83b72f8226a9e4dc02f3569b5117996d569b9e
Alexandre Bitar <joabitar76@gmail.com>
2021-03-26 13:43:37 -0300

    adicionado informações para a tela de inicio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>inicio | ignews</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Oi, bem vindo</span>
+          <h1>Novas notícias sobre o mundo <span>React</span></h1>
+          <p>Tenha acesso a todas as publicações<br/>
+          <span>por $9,90 mês</span></p>
+        </section>
+
+        <img  src="/images/avatar.svg" alt="garota codificando"/>
+      </main>
     </>
   )
 }
