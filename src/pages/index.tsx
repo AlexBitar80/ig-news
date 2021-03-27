38bebc4b8c2434989941259f8dc08b584058a478
Alexandre Bitar <joabitar76@gmail.com>
2021-03-27 12:49:26 -0300

    pegando valor do produto da API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import {SubscribeButton} from '../components/SubscribeButton'
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }:HomeProps) {
   return (
     <>
       <Head>
@@ -15,14 +24,32 @@ export default function Home() {
           <span>👋 Oi, bem vindo</span>
           <h1>Novas notícias sobre o mundo <span>React</span></h1>
           <p>Tenha acesso a todas as publicações<br/>
-            <span>por $9,90 mês</span>
+            <span>por {product.amount} mês</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img  src="/images/avatar.svg" alt="garota codificando"/>
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IZdgpGUYOPrFMD2qmBLJimO');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price.unit_amount / 100),
+  }
+  
+  return {
+    props: {
+      product
+    }
+  }
 }
   
