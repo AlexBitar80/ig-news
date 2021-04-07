a5c9ad505e04e7af5ab3f05ca1edc36f3526df85
Alexandre Bitar <joabitar76@gmail.com>
2021-04-07 17:05:24 -0300

    adicionado do link que está ativo

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+import Link, { LinkProps } from "next/link";
+import { ReactElement } from "react";
+import { cloneElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink({
+  children, 
+  activeClassName, 
+  ...props
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
+
+  const className = asPath === props.href ? activeClassName : '';
+  
+  return (
+    <Link {...props}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  );
+}
